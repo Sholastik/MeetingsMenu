276f61be5b2a7a0d869568cdb6652c7de7c06fd0
vyacheslavivanov <persikchicken@icloud.com>
2020-11-21 13:49:18 +0300

    Initial Commit

---
+++ MeetingsMenu/MeetingsMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetingsMenuApp.swift
+//  MeetingsMenu
+//
+//  Created by Вячеслав Иванов on 21.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct MeetingsMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
