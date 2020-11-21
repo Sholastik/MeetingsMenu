42a82c3875ead39368902c2933f37aaa2f53e28c
vyacheslavivanov <persikchicken@icloud.com>
2020-11-21 15:57:45 +0300

    Created Meeting class

---
+++ MeetingsMenu/Source/Meeting.swift
@@ -0,0 +1,26 @@
+//
+//  Meeting.swift
+//  MeetingsMenu
+//
+//  Created by Вячеслав Иванов on 21.11.2020.
+//
+
+import SwiftUI
+
+class Meeting {
+    let title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+    
+    func provideURL() -> URL? {
+        fatalError("Subclasses need to implement the 'provideURL()' method.")
+    }
+    
+    func joinMeeting() {
+        if let url = provideURL() {
+            NSWorkspace.shared.open(url)
+        }
+    }
+}
