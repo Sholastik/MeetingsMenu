01bb46b1722fc0eda652745711af65c5d9fe4475
vyacheslavivanov <persikchicken@icloud.com>
2020-11-21 16:13:40 +0300

    Created MeetingMenuItem class

---
+++ MeetingsMenu/Source/MeetingMenuItem.swift
@@ -0,0 +1,27 @@
+//
+//  MeetingMenuItem.swift
+//  MeetingsMenu
+//
+//  Created by Вячеслав Иванов on 21.11.2020.
+//
+
+import SwiftUI
+
+class MeetingMenuItem: NSMenuItem {
+    let meeting: Meeting
+    
+    init(index: Int, meeting: Meeting) {
+        self.meeting = meeting
+        
+        super.init(title: "\(index). \(self.meeting.title)", action: #selector(onClick(_:)), keyEquivalent: String(index))
+        self.target = self
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func onClick(_ sender: MeetingMenuItem) {
+        meeting.joinMeeting()
+    }
+}
