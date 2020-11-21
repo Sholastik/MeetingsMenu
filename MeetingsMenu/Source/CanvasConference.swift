3384d89c5bde8c9db7d7bf827a48cb375c6c226f
vyacheslavivanov <persikchicken@icloud.com>
2020-11-21 17:55:02 +0300

    Created Canvas Conference, Conferences objects

---
+++ MeetingsMenu/Source/CanvasConference.swift
@@ -0,0 +1,25 @@
+//
+//  Conference.swift
+//  MeetingsMenu
+//
+//  Created by Вячеслав Иванов on 21.11.2020.
+//
+
+import Foundation
+
+struct Conference: Codable {
+    let id: Int
+    let endedAt, startedAt: Date?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case endedAt = "ended_at"
+        case startedAt = "started_at"
+    }
+    
+    var isJoinable: Bool {
+        get {
+            return startedAt != nil && endedAt == nil
+        }
+    }
+}
---
+++ MeetingsMenu/Source/CanvasConferences.swift
@@ -0,0 +1,12 @@
+//
+//  Conferences.swift
+//  MeetingsMenu
+//
+//  Created by Вячеслав Иванов on 21.11.2020.
+//
+
+import Foundation
+
+struct Conferences: Codable {
+    let conferences: [Conference]
+}
